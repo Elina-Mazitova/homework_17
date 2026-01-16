0020c8c04cad65a6baef09b56967237a959e9985
Elina Mazitova <elinamazitova14@gmail.com>
2026-01-16 20:31:09 +0300

    Add test_put_product.py

---
+++ tests/test_put_product.py
@@ -0,0 +1,28 @@
+import json
+import requests
+from jsonschema import validate
+
+
+def test_put_product():
+    payload = {
+        "title": "Updated Product",
+        "price": 11.99,
+        "description":"Dog",
+        "category" : "Yellow",
+        "image" : "http://examples.com"
+    }
+
+    response = requests.put(
+        "https://fakestoreapi.com/products/1",
+        json=payload,
+        headers={"User-Agent": "Mozilla/5.0"}
+    )
+
+    assert response.status_code == 200
+
+    body = response.json()
+
+    with open("schemas/product_created_or_updated.json") as file:
+        schema = json.load(file)
+
+    validate(instance=body, schema=schema)
