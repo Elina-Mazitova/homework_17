67fe15a73de41d73aa99cdfabf8e1bac1449f755
Elina Mazitova <elinamazitova14@gmail.com>
2026-01-16 20:53:47 +0300

    Add test_put_product_incorrect

--- tests/test_put_product.py
+++ tests/test_put_product.py
@@ -26,3 +26,28 @@ def test_put_product():
         schema = json.load(file)
 
     validate(instance=body, schema=schema)
+
+
+def test_put_product_incorrect():
+    payload = {
+        "title": "Updated Product",
+        "price": 11.99,
+        "description":"Dog",
+        "category" : "Yellow",
+        "image" : "http://examples.com"
+    }
+
+    response = requests.put(
+        "https://fakestoreapi.com/products/1-",
+        json=payload,
+        headers={"User-Agent": "Mozilla/5.0"}
+    )
+
+    assert response.status_code == 400
+
+    body = response.json()
+
+    with open("schemas/product_updated_invalid.json") as file:
+        schema = json.load(file)
+
+    validate(instance=body, schema=schema)
