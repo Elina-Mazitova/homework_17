43930c60d9265ebee42c5327fc475aa8cf4c7c93
Elina Mazitova <elinamazitova14@gmail.com>
2026-01-16 20:41:23 +0300

    Add incorrect id to test_delete_product_incorrect_id

--- tests/test_delete_product.py
+++ tests/test_delete_product.py
@@ -8,3 +8,11 @@ def test_delete_product():
     )
 
     assert response.status_code == 200
+
+def test_delete_product_incorrect_id():
+    response = requests.delete(
+        "https://fakestoreapi.com/products/1-",
+        headers={"User-Agent": "Mozilla/5.0"}
+    )
+
+    assert response.status_code == 400
