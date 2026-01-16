908b0303c936dd8edb7fa5a802ba03c565ddd536
Elina Mazitova <elinamazitova14@gmail.com>
2026-01-16 20:25:56 +0300

    Add test_post_product.py

---
+++ tests/test_post_product.py
@@ -0,0 +1,28 @@
+import json
+import requests
+from jsonschema import validate
+
+
+def test_post_product():
+    payload = {
+        "title": "New Product",
+        "price": 29.99,
+        "description":"Cat",
+        "category" : "Red",
+        "image" : "http://example.com"
+    }
+
+    response = requests.post(
+        "https://fakestoreapi.com/products",
+        json=payload,
+        headers={"User-Agent": "Mozilla/5.0"}
+    )
+
+    assert response.status_code == 201
+
+    body = response.json()
+
+    with open("schemas/product_created.json") as file:
+        schema = json.load(file)
+
+    validate(instance=body, schema=schema)
